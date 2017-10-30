1d55810c98a84610f9e5a94280fb9dafd7042490
Findlay Smith <findlayksmith@gmail.com>
2017-10-30 12:52:57 +0000

    Update run.py

--- run.py
+++ run.py
@@ -1,10 +1,8 @@
+// ./run.py
 from flask import Flask
-
 app = Flask(__name__)
-
 @app.route('/')
 def index():
-  return "if u read this ur gay lol"
- 
-if __name__ == '__main__':
-  app.run()
+	return 'Yo, it's working!'
+if __name__ == "__main__":
+	app.run()
